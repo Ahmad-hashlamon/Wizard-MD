66cfa58204488691065c701b2f9e684048139aa4
Ahmad-hashlamon <111295592+Ahmad-hashlamon@users.noreply.github.com>
2022-10-02 11:26:43 +0300

    Update connection.js

--- Launcher/connection.js
+++ Launcher/connection.js
@@ -22,9 +22,9 @@ global.herokuapp = process.env.HEROKU_APP_NAME
 
 let fake = process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
-global.wame = "https://github.com/Ajmal-Achu/Wizard-MD"
-global.pemilik = ['918590508376']
-global.premium = ['918590508376']
+global.wame = "https://github.com/Ahmad-hashlamon/Wizard-MD"
+global.pemilik = ['970594182843']
+global.premium = ['970594182843']
 global.pengguna = process.env.USER_NAME
 global.footer = process.env.WATERMARK
 global.watermark = process.env.WATERMARK
